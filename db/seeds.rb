7d60255c95535a02e6404db34195387568f2c437
MirlaSantana <mirlasantanaf@gmail.com>
2015-05-13 20:44:35 -0300

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Local.create(name: 'teste', country: 'brasil', state: 'pi', city: 'picos', zipcode: '64600000')
+
+Project.create(name: 'teste', local_id: 1, size: 400, start_date: Time.now)
+
+Theme.create(project_id: 1, name: 'Teste 1')
+Theme.create(project_id: 1, name: 'Teste 2')
+Theme.create(project_id: 1, name: 'Teste 3')
+
+puts "dados inseridos..."
